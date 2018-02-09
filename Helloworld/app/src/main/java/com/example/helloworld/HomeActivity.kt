2d865766565f4995ae71de3f258cef9442a2adc9
韩承海 <374802597@qq.com>
2018-02-09 19:01:33 +0800

    no message

--- Helloworld/app/src/main/java/com/example/helloworld/HomeActivity.kt
+++ Helloworld/app/src/main/java/com/example/helloworld/HomeActivity.kt
@@ -2,12 +2,19 @@ package com.example.helloworld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.textviewlayout)
         print("hahahahahah")
     }
+
+    fun myClick(v: View) {
+        Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+    }
+
 }
--- Helloworld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Helloworld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.helloworld;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,9 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-        findViewById(R.id.button).setOnClickListener();
-
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void  myClick(View v) {
+        Toast.makeText(this,"click", Toast.LENGTH_LONG).show();
+    }
+
 }
