da97bf017742371695aab2a749ad177d5d07b83f
韩承海 <374802597@qq.com>
2018-02-13 14:06:40 +0800

    no message

--- Helloworld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ Helloworld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,18 +1,59 @@
 package com.example.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+
+    private Button mBtnTextView, mBtnButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        RadioGroup sexGroup = (RadioGroup)findViewById(R.id.sex_rg);
+//        sexGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+////
+//            public void onCheckedChanged(RadioGroup radioGroup, int chedkId) {
+//                if(chedkId == R.id.sex_man) {
+//                    Toast.makeText(MainActivity.this, "男", 0).show();
+//                } else {
+//                    Toast.makeText(MainActivity.this, "女", 0).show();
+//                }
+//            }
+//        });
+
+        mBtnTextView = findViewById(R.id.btn_textview);
+        mBtnTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 跳转到TextView 演示界面
+                Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mBtnButton = findViewById(R.id.btn_button);
+        mBtnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ButtonActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
 
     }
@@ -27,3 +68,20 @@ public class MainActivity extends Activity {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Helloworld/app/src/main/java/com/example/helloworld/TextViewActivity.java
@@ -0,0 +1,33 @@
+package com.example.helloworld;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class TextViewActivity extends AppCompatActivity {
+
+
+    private TextView mTv4, mTv5, mTv6;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view);
+        setContentView(R.layout.activity_text_view);
+
+        mTv4 = findViewById(R.id.tv_4);
+        mTv4.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG); //中划线
+        mTv4.getPaint().setAntiAlias(true);//去除锯齿
+
+        mTv5 = findViewById(R.id.tv_5);
+        mTv5.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);//下划线
+
+        mTv6 = findViewById(R.id.tv_6);//下划线
+        mTv6.setText(Html.fromHtml("<u>韩小醋飞了</u>"));
+    }
+}
