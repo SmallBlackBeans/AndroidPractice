37c3fa9313e5dd358fc0f9df77099adecf867d7f
韩承海 <374802597@qq.com>
2018-05-27 21:20:12 +0800

    no message

---
+++ Helloworld/app/src/main/java/com/example/helloworld/utils/UIUtils.java
@@ -0,0 +1,12 @@
+package com.example.helloworld.utils;
+
+/**
+ * @author 王维波
+ * @time 2016/9/22  16:39
+ * @desc ${TODD}
+ */
+public class UIUtils {
+    public static String getPackageName() {
+        return getPackageName();
+    }
+}
